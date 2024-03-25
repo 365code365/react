e25c176e93410e01abdc1a12b5ebadd25501df5b
io360 <614660823@qq.com>
2024-03-26 02:01:39 +0800

    change to vite build optimize build package

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -9,5 +9,7 @@ export default defineConfig({
         extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
     },
 
-
+    server: {
+        port: 3000
+    }
 })
