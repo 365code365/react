617ed31d91c62a2e28de35f159bf15426066eaf6
io360 <614660823@qq.com>
2024-04-01 22:52:21 +0800

    change to vite build optimize build package
    add login
    
    add import excel
    
    finish StudentManagement

--- src/component/StudentManagement.tsx
+++ src/component/StudentManagement.tsx
@@ -3,6 +3,7 @@ import {Table, Button, Modal, Form, Input, Select, Upload} from 'antd';
 import { FormInstance } from 'antd/lib/form';
 import * as XLSX from 'xlsx';
 import {UploadOutlined} from "@ant-design/icons";
+import {login} from "../api/loginApi";
 
 interface Student {
     UserID: number;
@@ -20,13 +21,13 @@ const StudentManagement: React.FC = () => {
 
     // Table columns configuration
     const columns = [
-        { title: 'UserID', dataIndex: 'UserID', key: 'UserID' },
+        // { title: 'UserID', dataIndex: 'UserID', key: 'UserID' },
         { title: 'FullName', dataIndex: 'FullName', key: 'FullName' },
         { title: 'Role', dataIndex: 'Role', key: 'Role' },
-        { title: 'Grade', dataIndex: 'grade', key: 'grade' },
-        { title: 'Email', dataIndex: 'email', key: 'email' },
+        { title: 'Grade', dataIndex: 'Grade', key: 'Grade' },
+        { title: 'Email', dataIndex: 'Email', key: 'Email' },
         { title: 'PasswordHash', dataIndex: 'PasswordHash', key: 'PasswordHash' },
-        { title: 'Gender', dataIndex: 'Gender', key: 'Gender', render: (gender: number) => (gender === 0 ? 'Female' : (gender === 1 ? 'Male' : 'Unknown')) },
+        { title: 'Gender', dataIndex: 'Gender', key: 'Gender', render: (gender: string) => (gender === '0' ? 'Female' : (gender === '1' ? 'Male' : 'Unknown')) },
         // Other columns...
     ];
 
@@ -44,6 +45,7 @@ const StudentManagement: React.FC = () => {
     };
 
     const onFinish = (values: any) => {
+        console.log('values',values)
         const newStudent: Student = {
             id: students.length + 1, // Generate new ID
             ...values,
@@ -64,9 +66,13 @@ const StudentManagement: React.FC = () => {
                 const sheetName = workbook.SheetNames[0];
                 const worksheet = workbook.Sheets[sheetName];
                 const excelData = XLSX.utils.sheet_to_json(worksheet, { header: 1 }) as any[][];
+
+                console.log('excelData',excelData)
                 // Assuming the first row contains headers
                 if (excelData.length > 1) {
                     const headers: string[] = excelData[0];
+                    console.log('headers',headers)
+                    console.log('headers.indexOf(\'Email\')',headers.indexOf('Email'))
                     const parsedStudents = excelData.slice(1).map((row: any, index: number) => ({
                         id: index + 1,
                         UserID: row[headers.indexOf('UserID')],
@@ -77,6 +83,7 @@ const StudentManagement: React.FC = () => {
                         PasswordHash: row[headers.indexOf('PasswordHash')],
                         Gender: parseInt(row[headers.indexOf('Gender')]), // Parse string value to integer
                     }));
+                    console.log('parsedStudents',parsedStudents)
                     setStudents([...students, ...parsedStudents]);
                 }
             }
@@ -109,9 +116,9 @@ const StudentManagement: React.FC = () => {
                     onFinish={onFinish}
                 >
                     <Form.Item
-                        label="Name"
-                        name="name"
-                        rules={[{ required: true, message: 'Please input student name!' }]}
+                        label="FullName"
+                        name="FullName"
+                        rules={[{ required: true, message: 'Please input student FullName!' }]}
                     >
                         <Input />
                     </Form.Item>
@@ -126,7 +133,7 @@ const StudentManagement: React.FC = () => {
 
                     <Form.Item
                         label="Grade"
-                        name="grade"
+                        name="Grade"
                         rules={[{ required: true, message: 'Please input student grade!' }]}
                     >
                         <Input />
@@ -134,29 +141,40 @@ const StudentManagement: React.FC = () => {
 
                     <Form.Item
                         label="Email"
-                        name="email"
+                        name="Email"
                         rules={[{ required: true, message: 'Please input student email!' }]}
                     >
                         <Input type="email" />
                     </Form.Item>
 
                     <Form.Item
-                        label="Password"
-                        name="password"
-                        rules={[{ required: true, message: 'Please input student password!' }]}
+                        label="PasswordHash"
+                        name="PasswordHash"
+                        rules={[{ required: true, message: 'Please input student PasswordHash!' }]}
                     >
                         <Input.Password />
                     </Form.Item>
 
                     <Form.Item
                         label="Gender"
-                        name="gender"
+                        name="Gender"
                         rules={[{ required: true, message: 'Please select student gender!' }]}
                     >
                         <Select>
                             <Select.Option value="0">Female</Select.Option>
                             <Select.Option value="1">Male</Select.Option>
                             <Select.Option value="2">Unknown</Select.Option>
+                        </Select>
+                    </Form.Item>
+                    <Form.Item
+                        label="Role"
+                        name="Role"
+                        rules={[{ required: true, message: 'Please select Role!' }]}
+                    >
+                        <Select>
+                            <Select.Option value="admin">admin</Select.Option>
+                            <Select.Option value="teacher">teacher</Select.Option>
+                            <Select.Option value="student">student</Select.Option>
                         </Select>
                     </Form.Item>
 
