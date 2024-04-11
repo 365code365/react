916b5c4c4608189df79f0f668a243f4c47f3d35b
io360 <614660823@qq.com>
2024-04-11 23:51:42 +0800

    change to vite build optimize build package
    add login
    
    add import excel
    
    finish StudentManagement

--- src/api/cert/cert.ts
+++ src/api/cert/cert.ts
@@ -1,5 +1,10 @@
-import {post} from "../../util/api";
+import {post, get} from "../../util/api";
 
 export const create = async <T>(data?: object): Promise<T> => {
     return post("/cert/create", data)
+}
+
+
+export const listAll = async <T>(): Promise<T> => {
+    return get("/cert/listAll")
 }
--- src/component/indexContaniner.tsx
+++ src/component/indexContaniner.tsx
@@ -1,11 +1,11 @@
-import {Button, Card, Modal, Popover, Steps} from "antd";
+import {Button, Card, message, Modal, Popover, Steps} from "antd";
 import {CheckCircleOutlined, LoadingOutlined} from "@ant-design/icons";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import ReactQuill from "react-quill";
 import HTMLPreview from "./HTMLPreview";
 import CreatCert from "./CreateCertForm";
 import CreateCertForm from "./CreateCertForm";
-import {create} from "../api/cert/cert";
+import {create, listAll} from "../api/cert/cert";
 
 
 const {Step} = Steps;
@@ -26,51 +26,54 @@ export const IndexContaniner = (props: any) => {
 
 
     const [cardInfoList, setCardInfoList] = useState([
-        {
-            id: 1,
-            title: 'Card 1',
-            desc: desc,
-            image: 'https://th.bing.com/th/id/OIP.QC-Su_ie-QzKBtY74XNNIgHaJ-?rs=1&pid=ImgDetMain',
-            currentStep: 0,
-            currentStepDesc: "waiting apply"
-        },
-        {
-            id: 2,
-            title: 'Card 1',
-            desc: desc,
-            image: 'https://th.bing.com/th/id/OIP.QC-Su_ie-QzKBtY74XNNIgHaJ-?rs=1&pid=ImgDetMain',
-            currentStep: 1,
-            currentStepDesc: "applying"
-        },
-
-        {
-            id: 3,
-            title: 'Card 1',
-            desc: desc,
-            image: 'https://th.bing.com/th/id/OIP.QC-Su_ie-QzKBtY74XNNIgHaJ-?rs=1&pid=ImgDetMain',
-            currentStep: 0,
-            currentStepDesc: "waiting apply"
-        },
-
-        {
-            id: 4,
-            title: 'Card 1',
-            desc: desc,
-            image: 'https://th.bing.com/th/id/OIP.QC-Su_ie-QzKBtY74XNNIgHaJ-?rs=1&pid=ImgDetMain',
-            currentStep: 2,
-            currentStepDesc: "waiting apply"
-        },
+        // {
+        //     id: 1,
+        //     title: 'Card 1',
+        //     desc: desc,
+        //     image: 'https://th.bing.com/th/id/OIP.QC-Su_ie-QzKBtY74XNNIgHaJ-?rs=1&pid=ImgDetMain',
+        //     currentStep: 0,
+        //     currentStepDesc: "waiting apply"
+        // },
+        // {
+        //     id: 2,
+        //     title: 'Card 1',
+        //     desc: desc,
+        //     image: 'https://th.bing.com/th/id/OIP.QC-Su_ie-QzKBtY74XNNIgHaJ-?rs=1&pid=ImgDetMain',
+        //     currentStep: 1,
+        //     currentStepDesc: "applying"
+        // },
+        //
+        // {
+        //     id: 3,
+        //     title: 'Card 1',
+        //     desc: desc,
+        //     image: 'https://th.bing.com/th/id/OIP.QC-Su_ie-QzKBtY74XNNIgHaJ-?rs=1&pid=ImgDetMain',
+        //     currentStep: 0,
+        //     currentStepDesc: "waiting apply"
+        // },
+        //
+        // {
+        //     id: 4,
+        //     title: 'Card 1',
+        //     desc: desc,
+        //     image: 'https://th.bing.com/th/id/OIP.QC-Su_ie-QzKBtY74XNNIgHaJ-?rs=1&pid=ImgDetMain',
+        //     currentStep: 2,
+        //     currentStepDesc: "waiting apply"
+        // },
 
     ]);
 
     const [isModalOpen, setIsModalOpen] = useState(false);
 
 
-    const [isAdmin, setIsAdmin] = useState(true)
+    useEffect(() => {
+        getListAll()
+    }, []);
 
-    const [isTeacher, setIsTeacher] = useState(true)
-
-    const [isStudent, setIsStudent] = useState(true)
+    const getListAll = async () => {
+        let res: any = await listAll()
+        setCardInfoList(res['data'])
+    }
 
 
     console.log('props', props)
@@ -91,46 +94,35 @@ export const IndexContaniner = (props: any) => {
             {cardInfoList.map((item) => (
                 <Card
                     style={{maxWidth: 400, margin: '10px', border: '0.5px solid red'}}
-                    cover={<img alt="Applying" src={item.image} style={{height: 300}}/>}
+                    cover={<img alt="Applying" src={item['CourseImage']} style={{height: 300}}/>}
                 >
-                    <Card.Meta title={<strong>{item.currentStepDesc}</strong>}/>
-                    <Steps size="small" status="error" current={item.currentStep} style={{marginTop: 20}}
-                           items={[
-                               {
-                                   title: 'Finished',
-                               },
-                               {
-                                   title: 'In Progress',
-                               },
-                               {
-                                   title: 'Waiting',
-                               },
-                           ]}
-                    />
-                    <div style={{marginBottom: '10px'}}>
+                    <Card.Meta title={<strong>{item['TitleOfCertification']}</strong>}/>
+                    <div style={{marginBottom: '10px', marginTop: '15px'}}>
                         <Popover placement="top" trigger="click"
                                  content={<div
                                      style={{width: '300px', maxHeight: '300px', textWrap: 'wrap', overflowY: 'auto'}}>
-                                     <HTMLPreview htmlContent={item.desc}/></div>}>
-                            <a>certificate desc</a>
+                                     <HTMLPreview htmlContent={item['CourseDesc']}/></div>}>
+                            <Button type={'primary'}>Certificate desc</Button>
                         </Popover>
                     </div>
-                    <div>
-                        <Popover content={<p>display reject reason</p>} trigger="click">
-                            <a style={{color: '#095672'}} type={"primary"}>show result</a>
-                        </Popover>
-                    </div>
-                    <Button type="primary">apply</Button>
-                    <Button style={{marginLeft: '10px', marginTop: '10px'}} type={'primary'}> apply cert</Button>
+                    <Button type="primary">Show My Apply</Button>
+                    <Button style={{marginLeft: '10px', marginTop: '10px'}} type={'primary'}> Apply Cert</Button>
                 </Card>
 
             ))}
             <CreateCertForm onCancel={() => {
                 setIsModalOpen(false)
-            }} onCreate={(values) => {
+            }} onCreate={async (values) => {
                 console.log('values', values)
-                let res = create(values)
-                console.log('cert create', res)
+                let res: any = await create(values)
+                console.log('res', res)
+                if (res['code'] == '00000') {
+                    getListAll()
+                    setIsModalOpen(false)
+                    message.info("create success")
+                } else {
+                    message.info(res['message'])
+                }
             }} key={'11'} visible={isModalOpen}/>
         </div>
 
