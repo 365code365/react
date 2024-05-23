0042a41882472784793a958fd1b5a8eade380d07
io360 <614660823@qq.com>
2024-05-23 22:00:23 +0800

    init

--- src/component/com/ProcessPage.tsx
+++ src/component/com/ProcessPage.tsx
@@ -11,7 +11,8 @@ interface ProcessPageProps {
 
 interface StepModel {
     aproveRole: string
-    order: string
+    order: number
+    desc: string
     status?: "wait" | "process" | "finish" | "error"; // Added status type
 }
 
@@ -27,6 +28,7 @@ const ProcessPage: React.FC<ProcessPageProps> = (props: ProcessPageProps) => {
 
     const [approveStatus, setApproveStatus] = useState<any>(null);
     const [approvalProcess, setApprovalProcess] = useState<StepModel[]>([]);
+    const [currentStep, setCurrentStep] = useState<number>(0);
 
     const getApplyListAll = async () => {
 
@@ -35,11 +37,6 @@ const ProcessPage: React.FC<ProcessPageProps> = (props: ProcessPageProps) => {
         }
         const res: any = await getListById(param);
         setApplyListOptions(res["data"])
-
-        const ruleRes: any = await getDetail({ID: props.CourseAndCertificationID});
-        const applyRuleJson = JSON.parse(ruleRes.data.applyRule);
-        console.log('ruleRes',applyRuleJson)
-        setApprovalProcess(applyRuleJson)
 
     };
 
@@ -52,6 +49,7 @@ const ProcessPage: React.FC<ProcessPageProps> = (props: ProcessPageProps) => {
             message.warning('Please select  approveStatus')
         }
         data['Status'] = approveStatus
+        data['role'] = localStorage.getItem("UserRole")
         console.log('values', data)
         const res: any = await updateCertClaim(data)
         if (res['code'] === '00000') {
@@ -92,6 +90,18 @@ const ProcessPage: React.FC<ProcessPageProps> = (props: ProcessPageProps) => {
             }
             setSelectedApply(resData);
             setListInfo(res['data']['documentList'])
+            const applyRuleJson = JSON.parse(res.data.applyRule);
+            console.log('ruleRes', applyRuleJson)
+
+            for (let i = 0; i < applyRuleJson.length; i++) {
+                if (applyRuleJson[i].status !== 'waiting') {
+                    setCurrentStep(parseInt(applyRuleJson[i].order))
+                    break
+                }
+            }
+
+            setApprovalProcess(applyRuleJson)
+
         } else {
             message.warning("apply is empty")
         }
@@ -103,100 +113,97 @@ const ProcessPage: React.FC<ProcessPageProps> = (props: ProcessPageProps) => {
         <Button type="primary" onClick={() => {
             getApplyListAll()
             handleApproval()
-        }}>Process</Button> <Modal
-        title="Approval Process"
-        visible={approveModalVisible}
-        onCancel={() => {
-            setApproveModalVisible(false)
-        }}
-        onOk={() => applyForm.submit()}
-        okText="Submit"
-    >
-        <Form
-            form={applyForm}
-            onFinish={handleApprovalSubmit}
-            layout="vertical"
-            initialValues={{CourseAndCertificationID: props.CourseAndCertificationID}} // Set initial value for CourseAndCertificationID
+        }}>Process</Button>
+        <Modal
+            width={900}
+            title="Approval Process"
+            visible={approveModalVisible}
+            onCancel={() => {
+                setApproveModalVisible(false)
+            }}
+            onOk={() => applyForm.submit()}
+            okText="Submit"
         >
-
-            <Form.Item>
-
-                <Steps
-                    current={0}>
-                    {approvalProcess.map((step, index) => (
-                        // <Step key={index} title={step.aproveRole} description={step.aproveRole}
-                        //       status={step.aproveRole}/>
-                        <Step key={index} title={step.aproveRole} description={step.aproveRole}
-                              status={step.status}/>
-                    ))}
-                </Steps>
-            </Form.Item>
-
-            <Form.Item
-                name="UserID"
-                label="UserID"
-                rules={[{required: true, message: 'Please enter total amount spent'}]}
+            <Form
+                form={applyForm}
+                onFinish={handleApprovalSubmit}
+                layout="vertical"
+                initialValues={{CourseAndCertificationID: props.CourseAndCertificationID}} // Set initial value for CourseAndCertificationID
             >
-                <Select
-                    defaultValue="Select an student"
-                    style={{minWidth: '200px'}}
-                    placeholder="Tags Mode"
-                    onChange={(e) => {
-                        handleChange(e)
-                    }}
-                    options={applyListOptions}/>
-            </Form.Item>
-            <Form.Item
-                name="Status"
-                label="Status"
-                rules={[{required: true, message: 'Please enter total amount spent'}]}
-            >
-                <Select
-                    defaultValue="Select an option"
-                    style={{minWidth: '200px'}}
-                    placeholder="Tags Mode"
-                    onChange={(e) => {
-                        handleApproveChange(e)
-                    }}
 
-                    options={[
-                        {label: 'Submit', value: 'Submit'},
-                        {label: 'Pending', value: 'Pending'},
-                        {label: 'Reject', value: 'Reject'},
-                        {label: 'approve', value: 'Finish'}
-                    ]}/>
-            </Form.Item>
-            <Form.Item
-                name="Remark"
-                label="Remark"
-            >
-                <Input.TextArea/>
-            </Form.Item>
-            {selectedApply && (
-                <div style={{padding: "20px"}}>
-                    <p><strong>Total Claim Amount:</strong> {selectedApply.TotalClaimAmount}</p>
-                    <p><strong>Total Amount Spent:</strong> {selectedApply.TotalAmountSpent}</p>
-                    <p><strong>Examination Date:</strong> {selectedApply.ExaminationDate}</p>
-                    <p><strong>Remark:</strong> {selectedApply.Remark}</p>
-                </div>
-            )}
-            {listInfo.length > 0 && (
-                <div style={{borderTop: '1px solid #ccc', paddingTop: '20px'}}>
-                    {listInfo.map((item: any, index: number) => (
-                        <div key={index} style={{display: 'flex', justifyContent: 'space-between'}}>
-                            <div>
-                                <p><strong>Title:</strong> {item.Title}</p>
-                                <p><strong>Description:</strong> {item.Description}</p>
-                                {/*<p><strong>Rejection Reason:</strong> {item.RejectionReason}</p>*/}
+                <Form.Item>
+
+                    <Steps
+                        current={currentStep}>
+                        {approvalProcess.map((step, index) => (
+                            <Step key={index} title={step.aproveRole} description={step.desc}
+                                  status={step.status}/>
+                        ))}
+                    </Steps>
+                </Form.Item>
+
+                <Form.Item
+                    name="UserID"
+                    label="UserID"
+                    rules={[{required: true, message: 'Please enter total amount spent'}]}
+                >
+                    <Select
+                        defaultValue="Select an student"
+                        style={{minWidth: '200px'}}
+                        placeholder="Tags Mode"
+                        onChange={(e) => {
+                            handleChange(e)
+                        }}
+                        options={applyListOptions}/>
+                </Form.Item>
+                <Form.Item
+                    name="Status"
+                    label="Status"
+                    rules={[{required: true, message: 'Please enter total amount spent'}]}
+                >
+                    <Select
+                        defaultValue="Select an option"
+                        style={{minWidth: '200px'}}
+                        placeholder="Tags Mode"
+                        onChange={(e) => {
+                            handleApproveChange(e)
+                        }}
+
+                        options={[
+                            {label: 'Pass', value: 'Pass'},
+                            {label: 'Reject', value: 'Reject'},
+                        ]}/>
+                </Form.Item>
+                <Form.Item
+                    name="Remark"
+                    label="Remark"
+                >
+                    <Input.TextArea/>
+                </Form.Item>
+                {selectedApply && (
+                    <div style={{padding: "20px"}}>
+                        <p><strong>Total Claim Amount:</strong> {selectedApply.TotalClaimAmount}</p>
+                        <p><strong>Total Amount Spent:</strong> {selectedApply.TotalAmountSpent}</p>
+                        <p><strong>Examination Date:</strong> {selectedApply.ExaminationDate}</p>
+                        <p><strong>Remark:</strong> {selectedApply.Remark}</p>
+                    </div>
+                )}
+                {listInfo.length > 0 && (
+                    <div style={{borderTop: '1px solid #ccc', paddingTop: '20px'}}>
+                        {listInfo.map((item: any, index: number) => (
+                            <div key={index} style={{display: 'flex', justifyContent: 'space-between'}}>
+                                <div>
+                                    <p><strong>Title:</strong> {item.Title}</p>
+                                    <p><strong>Description:</strong> {item.Description}</p>
+                                </div>
+                                <Image style={{width: "100px", height: "100px"}}
+                                       src={`data:image/jpeg;base64, ${item.FileContent}`}/>
                             </div>
-                            <Image style={{width: "100px", height: "100px"}}
-                                   src={`data:image/jpeg;base64, ${item.FileContent}`}/>
-                        </div>
-                    ))}
-                </div>
-            )}
-        </Form>
-    </Modal></>)
+                        ))}
+                    </div>
+                )}
+            </Form>
+        </Modal></>)
 }
 
 export default ProcessPage
